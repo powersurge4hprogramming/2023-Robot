67ace9562d047dadf777653f4ee6bb6b68bb907d
powersurge4hprogramming <powersurge4hstudent@gmail.com>
2023-01-11 20:46:42 -0500

    add simulation
     - simulate driving
     - add tank
     - add MathU

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,53 +4,83 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.Constants.DriveConstants;
+import frc.robot.utilities.MathU;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.simulation.ADXRS450_GyroSim;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
+import edu.wpi.first.wpilibj.simulation.EncoderSim;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotGearing;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotMotor;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotWheelSize;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
   // The motors on the left side of the drive.
-  private final MotorControllerGroup m_leftMotors =
-      new MotorControllerGroup(
-          new PWMSparkMax(DriveConstants.kLeftMotor1Port),
-          new PWMSparkMax(DriveConstants.kLeftMotor2Port));
+  private final MotorControllerGroup m_leftMotors = new MotorControllerGroup(
+      new PWMSparkMax(DriveConstants.kLeftMotor1Port),
+      new PWMSparkMax(DriveConstants.kLeftMotor2Port));
 
   // The motors on the right side of the drive.
-  private final MotorControllerGroup m_rightMotors =
-      new MotorControllerGroup(
-          new PWMSparkMax(DriveConstants.kRightMotor1Port),
-          new PWMSparkMax(DriveConstants.kRightMotor2Port));
+  private final MotorControllerGroup m_rightMotors = new MotorControllerGroup(
+      new PWMSparkMax(DriveConstants.kRightMotor1Port),
+      new PWMSparkMax(DriveConstants.kRightMotor2Port));
 
   // The robot's drive
   private final DifferentialDrive m_drive = new DifferentialDrive(m_leftMotors, m_rightMotors);
 
   // The left-side drive encoder
-  private final Encoder m_leftEncoder =
-      new Encoder(
-          DriveConstants.kLeftEncoderPorts[0],
-          DriveConstants.kLeftEncoderPorts[1],
-          DriveConstants.kLeftEncoderReversed);
+  private final Encoder m_leftEncoder = new Encoder(
+      DriveConstants.kLeftEncoderPorts[0],
+      DriveConstants.kLeftEncoderPorts[1],
+      DriveConstants.kLeftEncoderReversed);
 
   // The right-side drive encoder
-  private final Encoder m_rightEncoder =
-      new Encoder(
-          DriveConstants.kRightEncoderPorts[0],
-          DriveConstants.kRightEncoderPorts[1],
-          DriveConstants.kRightEncoderReversed);
+  private final Encoder m_rightEncoder = new Encoder(
+      DriveConstants.kRightEncoderPorts[0],
+      DriveConstants.kRightEncoderPorts[1],
+      DriveConstants.kRightEncoderReversed);
+
+  // These are our EncoderSim objects, which we will only use in
+  // simulation. However, you do not need to comment out these
+  // declarations when you are deploying code to the roboRIO.
+  private EncoderSim m_leftEncoderSim = new EncoderSim(m_leftEncoder);
+  private EncoderSim m_rightEncoderSim = new EncoderSim(m_rightEncoder);
 
   // The gyro sensor
-  private final Gyro m_gyro = new ADXRS450_Gyro();
+  private final ADXRS450_Gyro m_gyro = new ADXRS450_Gyro();
+
+  // Create the simulated gyro object, used for setting the gyro
+  // angle. Like EncoderSim, this does not need to be commented out
+  // when deploying code to the roboRIO.
+  private ADXRS450_GyroSim m_gyroSim = new ADXRS450_GyroSim(m_gyro);
 
   // Odometry class for tracking robot pose
   private final DifferentialDriveOdometry m_odometry;
+
+  // Create the simulation model of our drivetrain.
+  DifferentialDrivetrainSim m_driveSim = DifferentialDrivetrainSim.createKitbotSim(
+      KitbotMotor.kDualCIMPerSide, // 2 CIMs per side.
+      KitbotGearing.k7p31, // 7.31:1
+      KitbotWheelSize.kSixInch, // 6" diameter wheels.
+      VecBuilder.fill(0.001, 0.001, 0.001, 0.1, 0.1, 0.005, 0.005)); // TODO
+
+  private Field2d m_field = new Field2d();
 
   /** Creates a new DriveSubsystem. */
   public DriveSubsystem() {
@@ -64,16 +94,43 @@ public class DriveSubsystem extends SubsystemBase {
     m_rightEncoder.setDistancePerPulse(DriveConstants.kEncoderDistancePerPulse);
 
     resetEncoders();
-    m_odometry =
-        new DifferentialDriveOdometry(
-            m_gyro.getRotation2d(), m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
+    m_odometry = new DifferentialDriveOdometry(
+        m_gyro.getRotation2d(), m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
+
+    SmartDashboard.putData("Field", m_field);
+
   }
 
   @Override
   public void periodic() {
-    // Update the odometry in the periodic block
-    m_odometry.update(
-        m_gyro.getRotation2d(), m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
+    // This will get the simulated sensor readings that we set
+    // in the previous article while in simulation, but will use
+    // real values on the robot itself.
+    m_odometry.update(m_gyro.getRotation2d(),
+        m_leftEncoder.getDistance(),
+        m_rightEncoder.getDistance());
+    m_field.setRobotPose(m_odometry.getPoseMeters());
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // Set the inputs to the system. Note that we need to convert
+    // the [-1, 1] PWM signal to voltage by multiplying it by the
+    // robot controller voltage.
+    m_driveSim.setInputs(m_leftMotors.get() * RobotController.getInputVoltage(),
+        m_rightMotors.get() * RobotController.getInputVoltage());
+
+    // Advance the model by 20 ms. Note that if you are running this
+    // subsystem in a separate thread or have changed the nominal timestep
+    // of TimedRobot, this value needs to match it.
+    m_driveSim.update(0.02);
+
+    // Update all of our sensors.
+    m_leftEncoderSim.setDistance(m_driveSim.getLeftPositionMeters());
+    m_leftEncoderSim.setRate(m_driveSim.getLeftVelocityMetersPerSecond());
+    m_rightEncoderSim.setDistance(m_driveSim.getRightPositionMeters());
+    m_rightEncoderSim.setRate(m_driveSim.getRightVelocityMetersPerSecond());
+    m_gyroSim.setAngle(-m_driveSim.getHeading().getDegrees());
   }
 
   /**
@@ -116,9 +173,21 @@ public class DriveSubsystem extends SubsystemBase {
   }
 
   /**
+   * Drives the robot using tank controls.
+   *
+   * @param fwd the left movement
+   * @param rot the right movement
+   */
+  public void tankDrive(double left, double right, double max) {
+    left = MathU.signLerp(0, max, left);
+    right = MathU.signLerp(0, max, right);
+    m_drive.tankDrive(left, right);
+  }
+
+  /**
    * Controls the left and right sides of the drive directly with voltages.
    *
-   * @param leftVolts the commanded left output
+   * @param leftVolts  the commanded left output
    * @param rightVolts the commanded right output
    */
   public void tankDriveVolts(double leftVolts, double rightVolts) {
@@ -161,7 +230,8 @@ public class DriveSubsystem extends SubsystemBase {
   }
 
   /**
-   * Sets the max output of the drive. Useful for scaling the drive to drive more slowly.
+   * Sets the max output of the drive. Useful for scaling the drive to drive more
+   * slowly.
    *
    * @param maxOutput the maximum output to which the drive will be constrained
    */
---
+++ src/main/java/frc/robot/utilities/MathU.java
@@ -0,0 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.utilities;
+
+public class MathU {
+
+  /**
+   * Squares the input, preserving the sign
+   */
+  public static double squareInput(double input) {
+    return Math.pow(input, 2) * Math.signum(input);
+  }
+
+  /**
+   * Maps from 0 to 1 onto a to b
+   * tau is value from zero to one
+   * if a = 1, b = 6, tau = 0.5, then result = 3.5
+   */
+  public static final double lerp(double a, double b, double tau) {
+    tau = Math.max(tau, 0.0);
+    tau = Math.min(tau, 1.0);
+    return (b * tau) + (a * (1 - tau));
+  }
+
+  /**
+   * Maps from 0 to 1 onto a to b, preserving the sign of tau
+   * tau is value from zero to one
+   * if a = 1, b = 6, tau = 0.5, then result = 3.5
+   */
+  public static final double signLerp(double a, double b, double tau) {
+    double multiplier = Math.signum(tau);
+    tau = Math.max(Math.abs(tau), 0.0);
+    tau = Math.min(Math.abs(tau), 1.0);
+    return multiplier * (b * tau) + (a * (1 - tau));
+  }
+
+  /**
+   * Maps from a to b onto 0 to 1
+   * val is value from a to b
+   * if a = 1, b = 6, val = 3.5, then result = 0.5
+   */
+  public static final double invLerp(double a, double b, double val) {
+    /*
+     * val = Math.max(val, 0.2);
+     * val = Math.min(val, 0.8);
+     */
+    return (val - a) / (b - a);
+  }
+
+  /**
+   * Maps oldA and OldB to newA and newB
+   * val is value from oldA to oldB
+   * if a = 1, b = 6, val = 3.5, then result = 0.5
+   * result is from newA to newB
+   */
+  public static final double remap(double oldA, double oldB, double newA, double newB, double value) {
+    double result = invLerp(oldA, oldB, value);
+    return lerp(newA, newB, result);
+  }
+}
